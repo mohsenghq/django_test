5c0104f7c020ba4c340d217bf776a6dbd1575a45
mohsenghq <mohsenghq@gmail.com>
2025-03-20 19:38:47 +0330

    Refactor product views and templates; replace product_detail_view with product_view, add product_detail function, and update URL patterns and templates accordingly.

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import product_detail_view, ProductListView
+from .views import product_view, product_detail, ProductListView
 
 app_name = 'products'
 
 urlpatterns = [
-    path('products-fb', product_detail_view),
-    path('products-cbv', ProductListView.as_view()),
+    path('products-fb', product_view),
+    path('products-cb', ProductListView.as_view()),
+    path('products-fb/<int:pk>/', product_detail),
 ]
--- products/views.py
+++ products/views.py
@@ -1,21 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.views.generic import ListView
 
 # Create your views here.
 
-def product_detail_view(request):
+def product_view(request):
     products = Product.objects.all()
     context={
         'products': products
     }
-    return render(request, "products/product_detail.html", context)
+    return render(request, "products/product_view.html", context)
 
 
 class ProductListView(ListView):
-    template_name = "products/product_detail.html"
+    template_name = "products/product_view.html"
     queryset = Product.objects.all()
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         return context
+    
+
+def product_detail(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk)
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        'product': instance
+    }
+    return render(request, "products/product.html", context)
